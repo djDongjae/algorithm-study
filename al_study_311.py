79711570efc0da74f3424b7a096a8c9131a49858
Dong-Jae Oh <93889207+djDongjae@users.noreply.github.com>
2022-03-17 18:20:58 +0900

    Add files via upload

---
+++ al_study_311.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+count = 0
+group = 0
+
+for i in data:
+    count += 1
+    if i <= count:
+        group += 1
+        count = 0
+
+print(group)
