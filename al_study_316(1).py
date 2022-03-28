d29569b82f4df60adccf75676365303ea1cbfac0
Dong-Jae Oh <93889207+djDongjae@users.noreply.github.com>
2022-03-28 23:38:32 +0900

    Update al_study_316(1).py

--- al_study_316(1).py
+++ al_study_316(1).py
@@ -10,11 +10,11 @@ def solution(food_times, k):
         
         length = len(data)
         
-        while length * data[0][0] <= k:
+        while length * data[0][0] <= k: #남은 시간이 (배열의 길이 * 가장 적게 남은 음식) 보다 크거나 같아야함.
             min_value = heapq.heappop(data)[0]
-            for i in range(len(data)):
+            for i in range(len(data)):#돌아가면서 동일 최소값 빼기
                 data[i][0] -= min_value
-            k -= length * min_value
+            k -= length * min_value#현재 남은 시간에서 (배열의 길이 * 가장 적게 남은 음식)을 빼준다
             length -= 1
 
         data.sort(key=lambda x: x[1])
